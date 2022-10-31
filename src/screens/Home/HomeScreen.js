9a73e4443fbf3355f961fadfd73cc2a6e34fe446
Mandipbh <111733718+Mandipbh@users.noreply.github.com>
2022-10-31 18:12:09 +0530

    latest codde

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,16 +1,87 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {Component} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import DraggableFlatList from 'react-native-draggable-dynamic-flatlist';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Label, Title} from '../../components';
+import {moderatedScale, scale, theme} from '../../utils';
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>bghjkbg</Text>
-      <Text>kjbg</Text>
-    </View>
-  );
-};
+class HomeScreen extends Component {
+  state = {
+    data: [...Array(20)].map((d, index) => ({
+      key: `item-${index}`,
+      label: index,
+      backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${
+        index * 5
+      }, ${132})`,
+    })),
+  };
+
+  renderItem = ({item, index, move, moveEnd, isActive}) => {
+    return (
+      <TouchableOpacity
+        style={{
+          height: 100,
+          backgroundColor: isActive ? 'blue' : item.backgroundColor,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onLongPress={move}
+        onPressOut={moveEnd}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            color: 'white',
+            fontSize: 32,
+          }}>
+          {item.label}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <Icon name="timer-outline" size={scale(25)} />
+            <View style={styles.row}>
+              <Icon name="chevron-left" size={scale(25)} />
+              <Label title="CRONO TASKS" />
+              <Icon name="chevron-right" size={scale(25)} />
+            </View>
+          </View>
+          <DraggableFlatList
+            data={this.state.data}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => `draggable-item-${item.key}`}
+            scrollPercent={5}
+            onMoveEnd={({data}) => this.setState({data})}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.white,
+    paddingHorizontal: moderatedScale(15),
+  },
+  headerContainer: {
+    height: scale(55),
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+});
