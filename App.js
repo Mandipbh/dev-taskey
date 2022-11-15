eb29cbde22e9d200843af44687f224d228b30fc3
sagarbrainerhub <sagarp.brainerhub@gmail.com>
2022-11-15 18:53:14 +0530

    remove error logs

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {store, persistor} from './src/redux/Store';
 import {PersistGate} from 'redux-persist/integration/react';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomTab from './src/navigation/BottomTab';
 import MainStack from './src/navigation/MainStack';
+import {LogBox} from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    LogBox.ignoreLogs(['Warning: ... ']);
+    LogBox.ignoreAllLogs();
+  }, []);
   return (
     <NavigationContainer>
       <Provider store={store}>
